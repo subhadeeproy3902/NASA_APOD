03ed411e03954795673851a914eeb093be94e96c
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-04-02 18:42:27 +0530

    Done n dusted in 15 mins

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# NASA_APOD
+
+  # NASA Astronomy Picture of the Day 🌌
+
+![NASA APOD](https://apod.nasa.gov/apod/image/2404/CoronaExmouth_Hart_1920.jpg)
+
+## Detailed View of a Solar Eclipse Corona
+
+2nd April 2024
+
+### Explanation: 
+
+Only in the fleeting darkness of a total solar eclipse is the light of the solar corona easily visible. Normally overwhelmed by the bright solar disk, the expansive corona, the sun's outer atmosphere, is an alluring sight. But the subtle details and extreme ranges in the corona's brightness, although discernible to the eye, are notoriously difficult to photograph. Pictured here, however, using multiple images and digital processing, is a detailed image of the Sun's corona taken during the April 20, 2023 total solar eclipse from Exmouth, Australia. Clearly visible are intricate layers and glowing caustics of an ever changing mixture of hot gas and magnetic fields. Bright looping prominences appear pink just around the Sun's limb. A similar solar corona might be visible through clear skies in a narrow swath across the North America during the total solar eclipse that occurs just six days from today  NASA Coverage: Total Solar Eclipse of 2024 April 8
+
+> _Last Updated: April 2, 2024 (in GMT)_
---
+++ index.ts
@@ -0,0 +1,69 @@
+import { writeFileSync } from "fs";
+
+async function getAPOD() {
+  const apiKey = process.env.NASA_API_KEY || "DEMO_KEY";
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching APOD: ", error);
+    throw error;
+  }
+}
+
+// Get the current date suppose 2024-04-02 in the format 2nd April, 2024
+
+const currentDate = new Date();
+const day = currentDate.getDate();
+let daySuffix;
+if (day === 1 || day === 21 || day === 31) {
+  daySuffix = "st";
+} else if (day === 2 || day === 22) {
+  daySuffix = "nd";
+} else if (day === 3 || day === 23) {
+  daySuffix = "rd";
+} else {
+  daySuffix = "th";
+}
+
+const formattedDate = `${day}${daySuffix} ${currentDate.toLocaleString('default', { month: 'long' })} ${currentDate.getFullYear()}`;
+
+
+async function updateREADME(apodData: any) {
+  const formatterDate = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "long",
+  });
+  const timestamp = formatterDate.format(new Date());
+
+  const content = `
+  # NASA Astronomy Picture of the Day 🌌
+
+![NASA APOD](${apodData.hdurl})
+
+## ${apodData.title}
+
+${formattedDate}
+
+### Explanation: 
+
+${apodData.explanation}
+
+> _Last Updated: ${timestamp} (in GMT)_
+`;
+
+  writeFileSync("README.md", content);
+}
+
+async function main() {
+  try {
+    const apodData = await getAPOD();
+    await updateREADME(apodData);
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+}
+
+main();
